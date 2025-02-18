c48ccbfa84d836c285ca18b6d188e7e7728322e9
Yvan Roux <yvis99@gmail.com>
2025-02-18 21:41:47 +0100

    feat(Ingredient): enum and abstract class for generalization

---
+++ Assets/Scripts/Ingredients/AbstractIngredient.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class AbstractIngredient: MonoBehaviour
+{
+    public abstract IngredientType GetIngredientType();
+}
---
+++ Assets/Scripts/Ingredients/IngredientsEnum.cs
@@ -0,0 +1,8 @@
+public enum IngredientType
+{
+    Caldron = -1,
+    FrogSlime = 0,
+    LysFlower = 1,
+    Elixir1 = 2,
+    Elixir2 = 3
+}
