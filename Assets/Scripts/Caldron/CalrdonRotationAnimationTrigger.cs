e82e42f06cdc194273f90ef6c694a058f41f8b61
yvan <yvis99@gmail.com>
2025-04-24 21:21:52 +0200

    Caldron empty when lever is pulled

--- Assets/Scripts/Caldron/CalrdonRotationAnimationTrigger.cs
+++ Assets/Scripts/Caldron/CalrdonRotationAnimationTrigger.cs
@@ -1,46 +1,48 @@
 using UnityEngine;
 
-public class CalrdonRotationAnimationTrigger : MonoBehaviour
+public class CalrdonRotationAnimationTrigger : LeverAction
 {
     private static readonly int CaldronRotationTrigger = Animator.StringToHash("rotate");
-
-    [Tooltip("the rotation player has to give to trigger the rotation animation")]
-    [SerializeField] private float rotationThreshold = 20f;
-    [Space]
+    
     [SerializeField] private CaldronMerger caldronMerger;
     [SerializeField] private Transform caldronTransform;
-    [SerializeField] private DistanceReleaseGrabInteractable grabInteractable;
     [SerializeField] private Animator animator;
 
     private bool _animationIsRunning = false;
     private bool _animationHasStartAndIsNoMoreAtTheBeginning = false;
 
+    public override void LeverWasPulled()
+    {
+        StartAnimation();
+    }
+
     private void Update()
     {
         if (_animationIsRunning)
         {
-            grabInteractable.DetachInteractor();
+            if (caldronTransform.localRotation.x > 0.3)
+            {
+                _animationHasStartAndIsNoMoreAtTheBeginning = true;
+                caldronMerger.OnRotationMaxAngleReached();
+            }
+
+            // stop the animation, otherwise it's stuck in a loop
+            if (_animationHasStartAndIsNoMoreAtTheBeginning && caldronTransform.localRotation.x == 0)
+            {
+                animator.SetBool(CaldronRotationTrigger, false);
+                caldronMerger.OnRotationAnimationEnd(OnRotationFullyFinished);
+            }
         }
-        
-        if ( !_animationIsRunning && caldronTransform.localRotation.x > rotationThreshold/100)
+    }
+
+    private void StartAnimation()
+    {
+        if (!_animationIsRunning)
         {
             caldronMerger.OnRotationAnimationStart();
             animator.SetBool(CaldronRotationTrigger, true);
             _animationHasStartAndIsNoMoreAtTheBeginning = false;
             _animationIsRunning = true;
-        }
-
-        if (_animationIsRunning && transform.localRotation.x > 0.3)
-        {
-            _animationHasStartAndIsNoMoreAtTheBeginning = true;
-            caldronMerger.OnRotationMaxAngleReached();
-        }
-        
-        // stop the animation, otherwise it's stuck in a loop
-        if ( _animationIsRunning && _animationHasStartAndIsNoMoreAtTheBeginning && transform.rotation.x == 0)
-        {
-            animator.SetBool(CaldronRotationTrigger, false);
-            caldronMerger.OnRotationAnimationEnd(OnRotationFullyFinished);
         }
     }
 
