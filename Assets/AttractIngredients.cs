6fabd109c3b7a342a381173f70f125bffcde57ef
yvan <yvis99@gmail.com>
2025-05-23 15:15:49 +0200

    cuttingboard : ingredient are attracted by board

--- Assets/AttractIngredients.cs
+++ Assets/AttractIngredients.cs
@@ -1,16 +1,45 @@
+using System;
 using UnityEngine;
 
 public class AttractIngredients : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float attractionSpeed = 1f;
+    [SerializeField] private Transform positionToAttractTo;
+
+    /// <summary>
+    /// if actualGrabInteractable is not null, it means that an ingredient
+    /// is currently being attracted or on position on the cutting board
+    /// </summary>
+    private PersonalizedGrabInteractable actualIngredientGrabInteractable;
+    private Rigidbody actualIngredientRigidbody;
+    private Transform actualIngredientTransform;
+
+    private void OnTriggerEnter(Collider other)
     {
+        if (actualIngredientGrabInteractable is not null) return;
         
+        var grabInteractable = other.GetComponent<PersonalizedGrabInteractable>();
+        if (grabInteractable != null && !grabInteractable.IsGrabbed())
+        {
+            AttractNewIngredient(other.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (actualIngredientRigidbody is not null)
+        {
+            actualIngredientTransform.position = Vector3.Lerp(actualIngredientTransform.position, positionToAttractTo.position,
+                Time.deltaTime * attractionSpeed);
+        }
+    }
+
+    private void AttractNewIngredient(GameObject ingredientToGrab)
+    {
+        actualIngredientGrabInteractable = ingredientToGrab.GetComponent<PersonalizedGrabInteractable>();
+        actualIngredientRigidbody = ingredientToGrab.GetComponent<Rigidbody>();
+        actualIngredientTransform = ingredientToGrab.GetComponent<Transform>();
         
+        actualIngredientRigidbody.isKinematic = true;
     }
 }
